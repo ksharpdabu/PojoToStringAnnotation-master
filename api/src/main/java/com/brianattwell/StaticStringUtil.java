90a7b83a44a522d98402dcda7d80605dd3eabf1c
ksharpdabu123@gmail.com <@git1qazxsw2>
2016-03-24 11:40:16 +0800

    first commit

--- api/src/main/java/com/brianattwell/StaticStringUtil.java
+++ api/src/main/java/com/brianattwell/StaticStringUtil.java
@@ -1,6 +1,8 @@
 package com.brianattwell;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
@@ -9,4 +11,5 @@ import static java.lang.annotation.ElementType.TYPE;
  * Create a function in {@link StringUtil} for creating strings.
  */
 @Target(value = ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)  //默认是RetentionPolicy.CLASS，可以修改
 public @interface StaticStringUtil {}
